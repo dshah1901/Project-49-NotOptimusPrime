bf1b2d4bf1b76c7ae9e20cec620847e4f29aa888
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-10-11 17:00:00 +1300

    Removed Action bar.

--- NotOptimusPrime/app/src/main/java/com/example/notoptimusprime/MainActivity.java
+++ NotOptimusPrime/app/src/main/java/com/example/notoptimusprime/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.notoptimusprime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -19,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         //assign values to each control on the layout
