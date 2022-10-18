5525ff59edd8fc4608aafa675dcfe242fda03445
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-10-18 18:22:24 +1300

    List implemented

--- NotOptimusPrime/app/src/main/java/com/example/notoptimusprime/MainActivity.java
+++ NotOptimusPrime/app/src/main/java/com/example/notoptimusprime/MainActivity.java
@@ -2,21 +2,18 @@ package com.example.notoptimusprime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ListView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.example.notoptimusprime.model.DataProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
@@ -28,32 +25,42 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
+        //Testing the dictionary
+        Map<Integer, String> wordsDict = DataProvider.generateMaoriDigits();
+        List<String> words = new ArrayList<String>(wordsDict.values());
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(itemsAdapter);
+
+
+
         //assign values to each control on the layout
-        btn = findViewById(R.id.button);
+//        btn = findViewById(R.id.button);
 
         //Click listeners for button
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-                String url = "https://67ae-121-74-181-94.au.ngrok.io/normal_speech/F05_B1_C10_M2.wav";
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.println(error);
-                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                queue.add(stringRequest);
-
-            }
-        });
-    }
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+//                String url = "https://67ae-121-74-181-94.au.ngrok.io/normal_speech/F05_B1_C10_M2.wav";
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        System.out.println(error);
+//                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//                queue.add(stringRequest);
+//
+//            }
+//        }
+//        );
+ }
 }
