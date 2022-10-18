fb4cf7037c8618751881cfee4633b965734d9153
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-10-18 23:47:43 +1300

    Update DataProvider.java

--- NotOptimusPrime/app/src/main/java/com/example/notoptimusprime/model/DataProvider.java
+++ NotOptimusPrime/app/src/main/java/com/example/notoptimusprime/model/DataProvider.java
@@ -9,55 +9,51 @@ public class DataProvider {
     public static Map<Integer, String> generateSpeakers(){
         Map<Integer, String> speakers = new LinkedHashMap<>();
         speakers.put(1, "F02");
-        speakers.put(2, "F04");
-        speakers.put(3, "F05");
+        speakers.put(2, "F03");
+        speakers.put(3, "F04");
         speakers.put(4, "M01");
-        speakers.put(5, "M07");
-        speakers.put(6, "F05");
-        speakers.put(7, "M19");
-        speakers.put(8, "F17");
-        speakers.put(9, "M19");
+        speakers.put(5, "M05");
+        speakers.put(6, "M08");
+        speakers.put(7, "M09");
+        speakers.put(8, "M16");
         return speakers;
     }
     public static Map<Integer, String> generateCommands(){
         Map<Integer, String> commands = new LinkedHashMap<>();
-        commands.put(1, "Hello");
-        commands.put(2, "Bye");
-        commands.put(3, "IDK");
-        commands.put(4, "Why");
-        commands.put(5, "I");
-        commands.put(6, "Am");
-        commands.put(7, "Doing");
-        commands.put(8, "This");
-        commands.put(9, "scremmmms");
+        commands.put(1, "Sentence");
+        commands.put(2, "Left");
+        commands.put(3, "Said");
+        commands.put(4, "Enter");
+        commands.put(5, "Had");
+        commands.put(6, "Escape");
+        commands.put(7, "Upward");
+        commands.put(8, "More");
         return commands;
     }
 
     public static Map<Integer, String> generateIntelligibility(){
         Map<Integer, String> commands = new LinkedHashMap<>();
-        commands.put(1, "High");
-        commands.put(2, "Low");
-        commands.put(3, "Medium");
+        commands.put(1, "Low");
+        commands.put(2, "Very Low");
+        commands.put(3, "Mild");
         commands.put(4, "Very Low");
-        commands.put(5, "High");
-        commands.put(6, "Low");
-        commands.put(7, "Very Low");
-        commands.put(8, "Mild");
-        commands.put(9, "Mild");
+        commands.put(5, "Mild");
+        commands.put(6, "High");
+        commands.put(7, "High");
+        commands.put(8, "Low");
         return commands;
     }
 
     public static Map<Integer, String> generateOriginalFileName(){
         Map<Integer, String> commands = new LinkedHashMap<>();
-        commands.put(1, "F02_B1");
-        commands.put(2, "F02_B1");
-        commands.put(3, "F02_B1");
-        commands.put(5, "F02_B1");
-        commands.put(4, "F02_B1");
-        commands.put(6, "F02_B1");
-        commands.put(7, "F02_B1");
-        commands.put(8, "F02_B1");
-        commands.put(9, "F02_B1");
+        commands.put(1, "F02_B1_C12_M3.wav");
+        commands.put(2, "F03_B1_C18_M8.wav");
+        commands.put(3, "F04_B1_CW39_M8.wav");
+        commands.put(5, "M01_B1_C4_M6.wav");
+        commands.put(4, "M05_B1_CW27_M7.wav");
+        commands.put(6, "M08_B1_C6_M5.wav");
+        commands.put(7, "M09_B1_C16_M2.wav");
+        commands.put(8, "M16_B1_CW70_M3.wav");
         return commands;
     }
 
