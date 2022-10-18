a8bc88b072b4b246830076909c26542d7fbca4e5
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-10-18 22:34:54 +1300

    Done.

--- NotOptimusPrime/app/src/main/java/com/example/notoptimusprime/SpeakerDetailActivity.java
+++ NotOptimusPrime/app/src/main/java/com/example/notoptimusprime/SpeakerDetailActivity.java
@@ -2,14 +2,20 @@ package com.example.notoptimusprime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.example.notoptimusprime.R;
+import com.example.notoptimusprime.model.Speaker;
 
 public class SpeakerDetailActivity extends AppCompatActivity {
 
+    TextView tvSpeaker, tvIntelligibility, tvCommand;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +23,23 @@ public class SpeakerDetailActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_speaker_detail);
+
+        tvSpeaker = (TextView) findViewById(R.id.speaker_details);
+        tvIntelligibility = (TextView) findViewById(R.id.speaker_intelligibility_details);
+        tvCommand = (TextView) findViewById(R.id.speaker_command_details);
+
+        Intent thisIntent = getIntent();
+
+        //Use the book to populate the data into our views
+
+        Speaker speaker = (Speaker) thisIntent.getSerializableExtra(MainActivity.SPEAKER_DETAIL_KEY);
+        loadSpeaker(speaker);
+    }
+
+    private void loadSpeaker(Speaker speaker){
+        //Change activity title
+        tvSpeaker.setText(speaker.getSpeakerName());
+        tvIntelligibility.setText(speaker.getIntelligibility());
+        tvCommand.setText(speaker.getCommand());
     }
 }
